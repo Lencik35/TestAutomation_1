0e6755e4c0e699cb72a24fe6a6a389dff79921e5
admin <danielanice@mail.ru>
2025-01-23 21:18:18 +0200

    1 commit_1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 package md.tekwill;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
 
-    public static void main (String[] args){
-        System.out.println("Hello World!");
+public static void main(String[] args) {
+    System.out.println("Hello World!");
 
-        }
+}
 
 
