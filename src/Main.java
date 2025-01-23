65997e00f83e2e53e1edc641ab882edf4188bbeb
admin <danielanice@mail.ru>
2025-01-23 21:21:11 +0200

    23_01

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,6 @@
-package md.tekwill;
+public class Main {
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-
-
-public static void main(String[] args) {
-    System.out.println("Hello World!");
-
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
-
-
